4b7446ac3718de7e1db9055a6c036a6c486df6a5
Mikanwolfe <ivory@nekox.net>
2020-04-24 16:09:10 +1000

    PS4 complete, requires final writeup

--- problem-4/12b-io/12b-io/ifstream12.cpp
+++ problem-4/12b-io/12b-io/ifstream12.cpp
@@ -2,7 +2,7 @@
 
 // Design Patterns | Week 6 DSP | 101624964
 //  Why is the content harder than the concept?
-//  Adaptor design pattern seems easier than 12b-io.
+//  Adaptor design pattern seems easier than 12b-io!
 
 using namespace std;
 
@@ -22,20 +22,43 @@ void ifstream12::init()
 // "Reload" the buffer
 void ifstream12::reload()
 {
-	
+	init(); // Clear the buffer
+
+	// This function is called whenever the buffer is empty, and therefore 
+	//  we have to fill up the buffer through the intput stream
+	fIStream.read((char*)fBuffer, fBufferSize);
+
+	// There are two ways to count the number of characters in our buffer: 
+	//  Check the size of the fBuffer, or use fStream.gcount()
+	//  gcount refers to the number of characters returned in the last unformatted input-op.
+	//  It makes it simpler to use count here
+	fBytesAvailable = fIStream.gcount();
 }
 
 // Implements the mapping process, returns 0, 1, or -1. -1 = eof.
 int ifstream12::readBit()
 {
-	// Check for empty buffer, then eof.
+	// Check for empty buffer, and then check eof.
 	if (fBytesAvailable == 0)
 		reload();
 	if (eof())
 		return -1;
 
+	// Check the bit at fBitIndex, if the statement is true then the bit is 1, else 0.
+	int lResult = 0;
 	if ((fBuffer[fByteIndex] & (1 << fBitIndex)) != 0)
+		lResult =  1;
 
+	// Move the fBitIndex and account for changes once it has hit 0
+	if (--fBitIndex == -1) 
+	{
+		fBitIndex = 7;
+		fByteIndex++;
+		fBytesAvailable--;
+	}
+
+
+	return lResult;
 }
 
 
@@ -62,10 +85,22 @@ ifstream12::~ifstream12()
 
 ifstream12& ifstream12::operator>>(size_t& a12BitValue)
 {
-	
+	// Clear the 12-bit value
+	a12BitValue = 0;
 
+	// Creating a 12-bit value
+	for (size_t i = 0; i < 12; i++) 
+	{
+		// Reading in a bit (returns 0, 1, or -1)
+		int lBit = readBit();
 
+		if (lBit == -1)
+			break;
 
+		// Place the bit (lBit) in position i
+		a12BitValue = (lBit << (i)) + a12BitValue;
+
+	}
 	return *this;
 }
 
@@ -93,9 +128,9 @@ bool ifstream12::good() const
 	return fIStream.good();
 }
 
-// Checks of end of File := (Buffer Empty (=0) or FStream.EOF())
+// Checks of end of File := (Buffer Empty (=0) AND FStream.EOF())
 bool ifstream12::eof() const
 {
-	return ((fBytesAvailable == 0) || fIStream.eof());
+	return ((fBytesAvailable == 0) && fIStream.eof());
 }
 
